a9c11cfd6c502917aaaa09fbe61af2a0c65c2ae5
DeryGermann <termaiter@gmail.com>
2020-05-13 20:11:25 -0600

    SOMETHING WORKS

--- src/components/DodgeBlock.js
+++ src/components/DodgeBlock.js
@@ -1,34 +1,52 @@
-import React, {Component} from 'react';
-import {StyleSheet, Button, Text, View} from 'react-native';
-import Ball from './Ball';
+import React, { PureComponent } from "react";
+import { AppRegistry, StyleSheet, Dimensions, View } from "react-native";
+import { GameLoop } from "react-native-game-engine";
 
-export default class DodgeBlock extends Component {
-    constructor(props){
-        super(props);
-        this.state = {};
-    }
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+const RADIUS = 25;
 
-    render() {
-        return(
-            <View style={styles.container}>
-                <Text style={styles.header}>Dodge Block Screen</Text>
-                <Ball />
-            </View>
-        )
+export default class DodgeBlock extends PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      x: WIDTH / 2 - RADIUS,
+      y: HEIGHT / 2 - RADIUS
+    };
+  }
+
+  updateHandler = ({ touches, screen, layout, time }) => {
+    let move = touches.find(x => x.type === "move");
+    if (move) {
+      this.setState({
+        x: this.state.x + move.delta.pageX,
+        y: this.state.y + move.delta.pageY
+      });
     }
+  };
+
+  render() {
+    return (
+      <GameLoop style={styles.container} onUpdate={this.updateHandler}>
+
+        <View style={[styles.player, { left: this.state.x, top: this.state.y }]} />
+
+      </GameLoop>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#272727',
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    header: {
-        color: '#fff',
-        fontSize: 24,
-        fontWeight: '800',
-        textAlign: 'center'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+  },
+  player: {
+    position: "absolute",
+    backgroundColor: "pink",
+    width: RADIUS * 2,
+    height: RADIUS * 2,
+    borderRadius: RADIUS * 2
+  }
 });
+
+AppRegistry.registerComponent("BestGameEver", () => BestGameEver);
